8922f891727df7ba149ce3831321c83a18f4cb58
Keshav Rajput <you@example.com>
2019-05-20 16:29:13 -0500

    Trying something risky

---
+++ src/main/java/com/cricket/wc2019/Controllers/welcomeController.java
@@ -0,0 +1,14 @@
+package com.cricket.wc2019.Controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class welcomeController {
+
+    @RequestMapping("/welcome")
+    public String welcome()
+    {
+        return "Aaaomvn clean Bhosdikey";
+    }
+}
